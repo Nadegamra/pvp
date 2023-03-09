a433085eeeb294437b5b88fba64f493ea460fe26
artmit22 <artur.mitin@ktu.edu>
2023-03-09 16:41:43 +0200

    Deleted searchBar from registration branch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import AppRoutes from './components/routing/AppRoutes';
-import SearchBar from './components/searchBar/SearchBar.component';
 
 function App() {
     return (
         <div className="bg-bg-primary h-[100vh] w-full">
             <AppRoutes />
-            <SearchBar />
         </div>
     );
 }
