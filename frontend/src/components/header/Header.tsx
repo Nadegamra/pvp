de9eeeaec4b461f550db26863b61a053afa4620a
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-03-15 11:32:54 +0200

    Header improvements. Removed unnecessary CSS classes

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -26,116 +26,107 @@ function Header() {
     return (
         <div>
             {auth.user !== undefined ? (
-                <div className="w-[100%] h-[50px] bg-bg-secondary">
-                    <div className="flex flex-row h-[50px]">
-                        <div className="flex flex-row w-[50px] my-auto mr-[20px]">
-                            <div className="fixed top-[15px]">
-                                <button className="flex flex-row">
-                                    <div className="mr-[5px]"></div>
-                                    <span
-                                        className="material-symbols-outlined cursor-pointer select-none"
-                                        onClick={() => setState(state == 2 ? 0 : 2)}>
-                                        settings
+                <div className="h-[50px] bg-bg-secondary flex">
+                    <div className="flex-1"></div>
+                    <div className="mt-4 flex flex-col items-end">
+                        <button>
+                            <span
+                                className="material-symbols-outlined cursor-pointer select-none"
+                                onClick={() => setState(state == 2 ? 0 : 2)}>
+                                settings
+                            </span>
+                        </button>
+                        <button
+                            onClick={() => {
+                                toggleDarkMode(!(darkmode ?? false));
+                                setState(0);
+                            }}>
+                            {state === 2 && (
+                                <ul className="mt-4 bg-bg-secondary p-3">
+                                    {darkmode ?? false ? (
+                                        <span className="material-symbols-outlined align-middle pr-3">
+                                            light_mode
+                                        </span>
+                                    ) : (
+                                        <span className="material-symbols-outlined align-middle pr-3">
+                                            dark_mode
+                                        </span>
+                                    )}
+                                    Toggle {darkmode ?? false ? 'light' : 'dark'} mode
+                                </ul>
+                            )}
+                        </button>
+                    </div>
+                    <div className="w-[50px] ml-[40px] mr-[20px] mt-4">
+                        <button>
+                            <span
+                                className="material-symbols-outlined  cursor-pointer select-none"
+                                onClick={() => setState(state == 3 ? 0 : 3)}>
+                                manage_accounts
+                            </span>
+                        </button>
+                        {state === 3 && (
+                            <ul className="relative w-[120px] right-[70px] mt-4 bg-bg-secondary p-3">
+                                <Link onClick={() => setState(0)} to="/profile" className="pb-1">
+                                    <span className="material-symbols-outlined pr-3 align-middle">
+                                        person
                                     </span>
-                                </button>
+                                    Profile
+                                </Link>
                                 <button
-                                    className="flex flex-row"
+                                    className="pt-1"
                                     onClick={() => {
-                                        toggleDarkMode(!(darkmode ?? false));
+                                        auth.logout();
                                         setState(0);
                                     }}>
-                                    {state === 2 && (
-                                        <ul className="relative w-[200px] mt-[20px] bg-bg-secondary translate-x-[-80%] p-3">
-                                            {darkmode ?? false ? (
-                                                <span className="material-symbols-outlined align-middle pr-[10px]">
-                                                    light_mode
-                                                </span>
-                                            ) : (
-                                                <span className="material-symbols-outlined align-middle pr-[10px]">
-                                                    dark_mode
-                                                </span>
-                                            )}
-                                            Toggle {darkmode ?? false ? 'light' : 'dark'} mode
-                                        </ul>
-                                    )}
-                                </button>
-                            </div>
-                        </div>
-                        <div className="flex flex-row w-[50px] my-auto mr-[20px]">
-                            <div className="fixed top-[15px]">
-                                <button className="flex flex-row">
-                                    <div className="mr-[5px]"></div>
-                                    <span
-                                        className="material-symbols-outlined  cursor-pointer select-none"
-                                        onClick={() => setState(state == 3 ? 0 : 3)}>
-                                        manage_accounts
+                                    <span className="material-symbols-outlined pr-3 align-middle">
+                                        logout
                                     </span>
+                                    Logout
                                 </button>
-                                {state === 3 && (
-                                    <ul className="relative w-[120px] mt-[20px] bg-bg-secondary translate-x-[-60%] p-3">
-                                        <Link
-                                            onClick={() => setState(0)}
-                                            to="/profile"
-                                            className="pb-1">
-                                            <span className="material-symbols-outlined pr-3 align-middle">
-                                                person
-                                            </span>
-                                            <div className="inline">Profile</div>
-                                        </Link>
-                                        <button
-                                            className="pt-1"
-                                            onClick={() => {
-                                                auth.logout();
-                                                setState(0);
-                                            }}>
-                                            <span className="material-symbols-outlined pr-3 align-middle">
-                                                logout
-                                            </span>
-                                            <div className="inline">Logout</div>
-                                        </button>
-                                    </ul>
-                                )}
-                            </div>
-                        </div>
+                            </ul>
+                        )}
                     </div>
                 </div>
             ) : (
-                <div className="w-[100%] h-[50px] bg-bg-secondary static">
-                    <div className="flex flex-row h-[inherit]">
-                        <div className="w-[10%]"></div>
-                        <div className="w-[80%]"></div>
-                        <div className="flex flex-row w-[200px] my-auto">
-                            <Link to="/login" className="align-middle pr-[30px]">
-                                <ButtonText>Login</ButtonText>
-                            </Link>
-                            <Link to="/register" className="align-middle">
-                                <ButtonText>Register</ButtonText>
-                            </Link>
-                        </div>
-                        <div className="flex flex-row w-[50px] my-auto mr-[20px]">
-                            <div className="group fixed top-[15px]">
-                                <button className="flex flex-row">
-                                    <div className="mr-[5px]"></div>
-                                    <span className="material-symbols-outlined">settings</span>
-                                </button>
-                                <button
-                                    className="flex flex-row"
-                                    onClick={() => toggleDarkMode(!(darkmode ?? false))}>
-                                    <ul className="hidden group-hover:block relative w-[200px] mt-[20px] bg-bg-secondary translate-x-[-80%] p-3">
-                                        {darkmode ?? false ? (
-                                            <span className="material-symbols-outlined align-middle pr-[10px]">
-                                                light_mode
-                                            </span>
-                                        ) : (
-                                            <span className="material-symbols-outlined align-middle pr-[10px]">
-                                                dark_mode
-                                            </span>
-                                        )}
-                                        Toggle {darkmode ?? false ? 'light' : 'dark'} mode
-                                    </ul>
-                                </button>
-                            </div>
-                        </div>
+                <div className="h-[50px] bg-bg-secondary flex">
+                    <div className="flex-1"></div>
+                    <div className="my-auto">
+                        <Link to="/login" className="pr-[30px]">
+                            <ButtonText>Login</ButtonText>
+                        </Link>
+                        <Link to="/register" className="pr-[0px]">
+                            <ButtonText>Register</ButtonText>
+                        </Link>
+                    </div>
+                    <div className="mt-4 mx-7 flex flex-col items-end w-[30px]">
+                        <button>
+                            <span
+                                className="material-symbols-outlined cursor-pointer select-none"
+                                onClick={() => setState(state == 2 ? 0 : 2)}>
+                                settings
+                            </span>
+                        </button>
+                        <button
+                            onClick={() => {
+                                toggleDarkMode(!(darkmode ?? false));
+                                setState(0);
+                            }}>
+                            {state === 2 && (
+                                <ul className="relative w-[200px] mt-4 bg-bg-secondary p-3">
+                                    {darkmode ?? false ? (
+                                        <span className="material-symbols-outlined align-middle pr-3">
+                                            light_mode
+                                        </span>
+                                    ) : (
+                                        <span className="material-symbols-outlined align-middle pr-3">
+                                            dark_mode
+                                        </span>
+                                    )}
+                                    Toggle {darkmode ?? false ? 'light' : 'dark'} mode
+                                </ul>
+                            )}
+                        </button>
                     </div>
                 </div>
             )}
