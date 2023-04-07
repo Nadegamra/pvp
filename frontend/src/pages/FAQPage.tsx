d46f1bcd6471b51734c7de8ba6055377f71c6ab4
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-04-07 21:11:02 +0300

    Added FAQ page

--- frontend/src/pages/FAQPage.tsx
+++ frontend/src/pages/FAQPage.tsx
@@ -1,141 +1,148 @@
+import FaqCard from '../components/faq/FaqCard';
+
 function FAQPage() {
     return (
-        <div>
+        <div className="pb-10">
             <div className="text-fs-heading font-bold m-5">Dažnai užduodami klausimai</div>
-            <div className="rounded-t-lg border mx-5">
-                <h2 className="mb-0" id="headingOne">
-                    <button
-                        className="group flex w-full items-center rounded-t-[15px] border-0 bg-white py-4 px-5 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
-                        type="button"
-                        data-te-collapse-init
-                        data-te-target="#collapseOne"
-                        aria-expanded="true"
-                        aria-controls="collapseOne">
-                        Accordion Item #1
-                        <span className="ml-auto -mr-1 h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="h-6 w-6">
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                                />
-                            </svg>
-                        </span>
-                    </button>
-                </h2>
-                <div
-                    id="collapseOne"
-                    data-te-collapse-item
-                    data-te-collapse-show
-                    aria-labelledby="headingOne"
-                    data-te-parent="#accordionExample">
-                    <div className="py-4 px-5">
-                        <strong>This is the first item's accordion body.</strong> It is shown by
-                        default, until the collapse plugin adds the appropriate classNamees that we
-                        use to style each element. These classNamees control the overall appearance,
-                        as well as the showing and hiding via CSS transitions. You can modify any of
-                        this with custom CSS or overriding our default variables. It's also worth
-                        noting that just about any HTML can go within the{' '}
-                        <code>.accordion-body</code>, though the transition does limit overflow.
-                    </div>
-                </div>
-            </div>
-            <div className="border mx-5">
-                <h2 className="mb-0" id="headingTwo">
-                    <button
-                        className="group relative flex w-full items-center rounded-none border-0 bg-white py-4 px-5 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
-                        type="button"
-                        data-te-collapse-init
-                        data-te-collapse-collapsed
-                        data-te-target="#collapseTwo"
-                        aria-expanded="false"
-                        aria-controls="collapseTwo">
-                        Accordion Item #2
-                        <span className="ml-auto -mr-1 h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="h-6 w-6">
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                                />
-                            </svg>
-                        </span>
-                    </button>
-                </h2>
-                <div
-                    id="collapseTwo"
-                    className="hidden"
-                    data-te-collapse-item
-                    aria-labelledby="headingTwo"
-                    data-te-parent="#accordionExample">
-                    <div className="py-4 px-5">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by
-                        default, until the collapse plugin adds the appropriate classNamees that we
-                        use to style each element. These classes control the overall appearance, as
-                        well as the showing and hiding via CSS transitions. You can modify any of
-                        this with custom CSS or overriding our default variables. It's also worth
-                        noting that just about any HTML can go within the{' '}
-                        <code>.accordion-body</code>, though the transition does limit overflow.
-                    </div>
-                </div>
-            </div>
-            <div className="rounded-b-lg border mx-5">
-                <h2 className="accordion-header mb-0" id="headingThree">
-                    <button
-                        className="group relative flex w-full items-center border-0 bg-white py-4 px-5 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)] [&[data-te-collapse-collapsed]]:rounded-b-[15px] [&[data-te-collapse-collapsed]]:transition-none"
-                        type="button"
-                        data-te-collapse-init
-                        data-te-collapse-collapsed
-                        data-te-target="#collapseThree"
-                        aria-expanded="false"
-                        aria-controls="collapseThree">
-                        Accordion Item #3
-                        <span className="ml-auto -mr-1 h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                className="h-6 w-6">
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                                />
-                            </svg>
-                        </span>
-                    </button>
-                </h2>
-                <div
-                    id="collapseThree"
-                    className="hidden"
-                    data-te-collapse-item
-                    aria-labelledby="headingThree"
-                    data-te-parent="#accordionExample">
-                    <div className="py-4 px-5">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by
-                        default, until the collapse plugin adds the appropriate classes that we use
-                        to style each element. These classes control the overall appearance, as well
-                        as the showing and hiding via CSS transitions. You can modify any of this
-                        with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>,
-                        though the transition does limit overflow.
-                    </div>
-                </div>
-            </div>
+            <FaqCard
+                id={1}
+                pos="top"
+                buttonText="Kaip užtikrinama puslapio duomenų apsauga?"
+                content="Ji yra užtikrinama Europos Sąjungos 
+                        teisės aktais,bendruoju duomenų apsaugos 
+                        reglamentu, nacionalinės duomenų apsaugos 
+                        institucijomis ir jų veikimo principais."
+            />
+            <FaqCard
+                id={2}
+                pos="middle"
+                buttonText="Kaip organizuojamas pasirinkto varianto pristatymas?"
+                content="Pristatymai organizuojami kurjerių 
+                        pagalba atsiunčiant siuntą tiesiai į 
+                        namų paštą arba atitinkamą paštomatą."
+            />
+            <FaqCard
+                id={3}
+                pos="middle"
+                buttonText="Ką daryti jei pasirinkto varianto pristatymas vėluoja?"
+                content="Tokiu atveju galite palaukti iki 30 
+                        minučių. Jei siunta vėluoja daugiau nei 
+                        30 minučių prašykite kompensacijos."
+            />
+            <FaqCard
+                id={4}
+                pos="middle"
+                buttonText="Ar saugu naudotis elektronine bankininkyste jūsų svetainėje? Kokie taikomi saugumo reikalavimai?"
+                content="Taip , saugu . Mūsų tinklapis bus susietas 
+                        su nacionaliniu kibernetinio saugumo centru bei su 
+                        valstybinės vartotojų tesių apsaugos tarnybos tinklapiu."
+            />
+            <FaqCard
+                id={5}
+                pos="middle"
+                buttonText="Koks jūsų svetainės tikslas?"
+                content="Mūsų tikslas yra kuriamas interneto puslapis, 
+                        skirtas tarpininkauti tarp žmonių, kurie nori 
+                        skolinti įrenginius ir įmonių, kurios skolina 
+                        įrenginius, o pamatuojamas poveikis atitinka 
+                        rezultatą, kad skolinančios įmonės turės daugiau 
+                        daiktų, kuriuos galės skolinti, o žmonės, kurie 
+                        turi retai naudojamų įrenginių juos skolins ir 
+                        iš jų užsidirbs."
+            />
+            <FaqCard
+                id={6}
+                pos="middle"
+                buttonText="Per kiek laiko bus gautas užsakymas?"
+                content="Per 3 darbo dienas."
+            />
+            <FaqCard
+                id={7}
+                pos="middle"
+                buttonText="Kokius vėžėjus prekėms atsiųsti naudojate?"
+                content="Naudojami visi Lietuvoje veikiantys kurjeriai."
+            />
+            <FaqCard
+                id={8}
+                pos="middle"
+                buttonText="Ar įmanoma sekti kliento užsakymą?"
+                content="Taip, internetinėje savo paskyroje galite."
+            />
+            <FaqCard
+                id={9}
+                pos="middle"
+                buttonText="Ar taikomos ir naudojamos nuolaidos arba įvairios lengvatos, dovanų kuponai?"
+                content="Taikomos  nuolaidos senesnių kartų konsolėms, naudotiems 
+                        įrenginiams, bei prasčiau veikiantiems įrenginiams."
+            />
+            <FaqCard
+                id={10}
+                pos="middle"
+                buttonText="Kokie yra prekių įkainiai?"
+                content="Jie konkrečiai priklauso nuo atitinkamos prekės."
+            />
+            <FaqCard
+                id={11}
+                pos="middle"
+                buttonText="Ką daryti jei klientas nori pakeisti užsakymą?"
+                content="Užsakymą pakeisti t.y atšaukti galima ne 
+                        vėliau nei siunta yra pradėta siųsti klientui."
+            />
+            <FaqCard
+                id={12}
+                pos="middle"
+                buttonText="Ar klientas gali pakeisti arba nutraukti 
+                            užsakymą jei siunta jau paruošta arba keliauja?"
+                content="Užsakymą pakeisti t.y atšaukti galima ne 
+                        vėliau nei siunta yra pradėta siųsti klientui."
+            />
+            <FaqCard
+                id={13}
+                pos="middle"
+                buttonText="Ką daryti jei siunta neatkeliavo?"
+                content="Tokiu atveju galite palaukti iki 30 minučių, 
+                        nes negausite kompensacijos iki tokio laiko. 
+                        Jei siunta vėluoja daugiau nei 30 minučių 
+                        prašykite kompensacijos."
+            />
+            <FaqCard
+                id={14}
+                pos="middle"
+                buttonText="Ar įmanomas grąžinimas ir pinigų atgavimas?"
+                content="Tokiu atveju galite palaukti iki 30 minučių, 
+                        nes negausite kompensacijos iki tokio laiko. 
+                        Jei siunta vėluoja daugiau nei 30 minučių 
+                        prašykite kompensacijos."
+            />
+            <FaqCard
+                id={15}
+                pos="middle"
+                buttonText="Ar galima pasiskolinti įrenginius ir jei taip ar įmanoma ilgesniam laikotarpiui nei numatyta sutartyje?"
+                content="Ne, nebent primokama atitinkama dalis prieš gaunant įrenginį"
+            />
+            <FaqCard
+                id={16}
+                pos="bottom"
+                buttonText="Kokias taisykles taikote prekių pristatyme ir grąžinime?"
+                content="Klientas atvykęs atsiimti išsinuomotos žaidimo konsoles 
+                        atvyksta pasiimti pats, jei klientas issinuomavo žaidimo 
+                        konsolė 1 parai, kaip pavyzdys 12:00 valandą, tai sekančią 
+                        dieną gali gražinti iki 15:00 valandos, mes duodame laiko 
+                        klientui, kad būtų patogu gražinti. Dažniausiai prisitaikome 
+                        prie kliento, kaip jam tinka grąžinti.
+                        Pratęsimas yra galimas, bet klientų prašome is anksto apie tai  
+                        pranešti, tai yra ne paskutinę minutę, kadangi galbūt yra jau 
+                        yra laukiantys kiti klientai eilėje. Dažniausiai pratęsimo mokestį 
+                        nuimame nuo depozito ir grąžiname mažesni depozitą dėl kliento patogumo, 
+                        kad jam nereiktų galvoti dėl papildomos nuomos kainos.
+                        Viskas priklauso nuo to, kuriam laikotarpiui išnuomota konsolė. 
+                        Gali būti 2,4,10 parų ir pan. Grąžinimo dieną konkrečios valandos 
+                        neturime. Daiktas grąžinamas kuomet abi puses turi laiko. Mūsų 
+                        trumpiausias bazinis konsolės skolinimosi terminas yra 1 para. 
+                        Kaikurių pigių konsolių 2 paros. Klientui paliekama galimybė 
+                        prasitesti nuomos laikotarpį tiek, kiek jis nori jeigu konsolė 
+                        nėra rezervuota kito kliento."
+            />
         </div>
     );
 }
