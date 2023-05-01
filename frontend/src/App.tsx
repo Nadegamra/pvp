1b74064a8e3bc8e212a31e1428b3b6605345e317
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-05-02 00:39:56 +0300

    Disable footer because it is unnecessary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,9 +15,7 @@ function App() {
             <div className="flex-1" onClick={() => header.hideAll()}>
                 <AppRoutes />
             </div>
-            <div onClick={() => header.hideAll()}>
-                <Footer />
-            </div>
+            <div onClick={() => header.hideAll()}>{/* <Footer /> */}</div>
         </div>
     )
 }
