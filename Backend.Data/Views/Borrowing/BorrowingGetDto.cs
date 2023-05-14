f90b66c387ae1ce103422723b275e5f133eb82fc
Karolis Žukauskas <karolis.zukaus@gmail.com>
2023-05-15 02:00:03 +0300

    Finished Borrowings backend (not tested yet)

--- Backend.Data/Views/Borrowing/BorrowingGetDto.cs
+++ Backend.Data/Views/Borrowing/BorrowingGetDto.cs
@@ -1,4 +1,5 @@
 ﻿using Backend.Data.Models;
+using Backend.Data.Views.UserConsole;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace Backend.Data.Views.BorrowedConsole
         public int UserId { get; set; }
         public int ConversationId { get; set; }
         public BorrowingStatus Status { get; set; }
+        public ICollection<UserConsoleGetDto> UserConsoles { get; set; }
     }
 }
--- Backend/Handlers/BorrowingsHandler.cs
+++ Backend/Handlers/BorrowingsHandler.cs
@@ -3,6 +3,9 @@ using Backend.Data;
 using Backend.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Backend.Data.Views.BorrowedConsole;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Backend.Data.Views.UserConsole;
 
 namespace Backend.Handlers
 {
@@ -11,41 +14,81 @@ namespace Backend.Handlers
         private readonly IMapper _mapper;
         private readonly AppDbContext _context;
         private readonly UserManager<User> _userManager;
+        private readonly UserConsolesHandler _userConsolesHandler;
 
-        public BorrowingsHandler(IMapper mapper, AppDbContext context, UserManager<User> userManager)
+        public BorrowingsHandler(IMapper mapper, AppDbContext context, UserManager<User> userManager, UserConsolesHandler userConsolesHandler)
         {
             _mapper = mapper;
             _context = context;
             _userManager = userManager;
+            _userConsolesHandler = userConsolesHandler;
         }
 
         public async Task<List<BorrowingGetDto>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return _mapper.Map<List<Borrowing>, List<BorrowingGetDto>>(await _context.Borrowings.Include(x => x.UserConsoles).ThenInclude(x => x.Images).Include(x => x.UserConsoles).ThenInclude(x => x.Console).ToListAsync());
         }
-        public async Task<List<BorrowingGetDto>> GetByUserAsync()
+        public async Task<List<BorrowingGetDto>> GetByUserAsync(ClaimsPrincipal userClaims)
         {
-            throw new NotImplementedException();
+            User user = await _userManager.GetUserAsync(userClaims);
+            return _mapper.Map<List<Borrowing>, List<BorrowingGetDto>>(await _context.Borrowings.Include(x => x.UserConsoles).ThenInclude(x => x.Images).Include(x => x.UserConsoles).ThenInclude(x => x.Console).Where(x=>x.UserId == user.Id).ToListAsync());
         }
         public async Task<BorrowingGetDto> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return _mapper.Map<Borrowing, BorrowingGetDto>(await _context.Borrowings.Include(x => x.UserConsoles).ThenInclude(x => x.Images).Include(x => x.UserConsoles).ThenInclude(x => x.Console).Where(x=>x.Id == id).FirstAsync());
         }
         public async Task AddAsync(BorrowingAddDto addDto)
         {
+            async Task UpdateUserConsole(int id)
+            {
+                var userConsole = await _context.UserConsoles.Where(x => x.Id == id).FirstAsync();
+
+                userConsole.BorrowedConsoleId = id;
+
+                _context.UserConsoles.Update(userConsole);
+                await _context.SaveChangesAsync();
+            }
+
+            foreach(var id in addDto.UserConsoleIds)
+            {
+                await _userConsolesHandler.UpdateStatus(new UserConsoleStatusUpdateDto { Id= id, ConsoleStatus = UserConsoleStatus.AT_LENDER });
+                await UpdateUserConsole(id);
+            }
             throw new NotImplementedException();
         }
+
         public async Task UpdateAsync(BorrowingUpdateDto updateDto)
         {
-            throw new NotImplementedException();
+            var borrowing = await _context.Borrowings.Where(x=>x.Id == updateDto.Id).Include(x=>x.UserConsoles).FirstAsync();
+
+            var originalUserConsoleIds = borrowing.UserConsoles.Select(x=>x.Id).ToList();
+
+            foreach(var id in originalUserConsoleIds)
+            {
+                await _userConsolesHandler.UpdateStatus(new UserConsoleStatusUpdateDto { Id = id, ConsoleStatus = UserConsoleStatus.AT_PLATFORM });
+            }
+
+            foreach(var id in updateDto.UserConsoleIds)
+            {
+                await _userConsolesHandler.UpdateStatus(new UserConsoleStatusUpdateDto { Id = id, ConsoleStatus = UserConsoleStatus.AT_LENDER });
+            }
         }
         public async Task UpdateStatusAsync(BorrowingUpdateStatusDto statusDto)
         {
-            throw new NotImplementedException();
+            var borrowing = await _context.Borrowings.Where(x => x.Id == statusDto.Id).FirstAsync();
+
+            borrowing.Status = statusDto.Status;
+
+            _context.Borrowings.Update(borrowing);
+            await _context.SaveChangesAsync();
         }
         public async Task DeleteAsync(BorrowingDeleteDto deleteDto)
         {
-            throw new NotImplementedException();
+            
+            var borrowing = await _context.Borrowings.Where(x=>x.Id ==deleteDto.Id).FirstAsync();
+
+            _context.Borrowings.Remove(borrowing);
+            await _context.SaveChangesAsync();
         }
     }
 }
