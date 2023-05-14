2ee17e6918983598676f7fd329b81a0918311ff5
artmit22 <artur.mitin@ktu.edu>
2023-05-15 00:01:14 +0300

    Added available amount and accessories when clicking on console

--- frontend/src/pages/ConsolesPage.tsx
+++ frontend/src/pages/ConsolesPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { ConsoleGet } from '../models/Console'
+import { UserConsoleGet, ConsoleStatus, UserConsolesStatusRequest } from '../models/UserConsole'
 import { getConsoles } from '../api/ConsolesApi'
 import { imagePathToURL } from '../models/Image'
 import { Link } from 'react-router-dom'
@@ -7,7 +8,8 @@ import { useTranslation } from 'react-i18next'
 
 function ConsolesPage() {
     const { t } = useTranslation()
-    const [consoles, setConsoles] = useState<ConsoleGet[]>()
+    const [consoles, setConsoles] = useState<UserConsoleGet[]>()
+    const [expandedConsole, setExpandedConsole] = useState<UserConsoleGet>();
 
     useEffect(() => {
         getConsoles().then((response) => {
@@ -16,14 +18,46 @@ function ConsolesPage() {
     }, [])
     return (
       <div className="grid grid-rows-2 grid-cols-5">
-          {consoles?.map((console) => (
-              <div className="bg-bg-secondary rounded-lg w-[250px] p-3 m-3 cursor-pointer select-none">
-                  {console.images.length > 0 && <img src={imagePathToURL(console.images[0].path, 250)} alt={console.images[0].name} />}
-                  <div className="text-t-secondary text-center">{console.name} ({console.dailyPrice} eur)</div>                  
+          {consoles?.map((userConsole) => (
+              <div className="bg-bg-secondary rounded-lg w-[250px] p-3 m-3 cursor-pointer select-none"
+            onClick={() => setExpandedConsole(userConsole)}>
+                  {userConsole.images.length > 0 && <img src={imagePathToURL(userConsole.images[0].path, 250)} alt={userConsole.images[0].name} />}
+                  <div className="text-t-secondary text-center">{userConsole?.console.name} ({userConsole?.console.dailyPrice} eur)</div>                  
               </div>
           ))}
+          {expandedConsole && (
+      <ConsoleDetails
+        userConsole={expandedConsole}
+        onClose={() => setExpandedConsole(undefined)}
+      />
+    )}
       </div>
   )
 }
+
+interface ConsoleDetailsProps {
+    userConsole: UserConsoleGet;
+    onClose: () => void;
+  }
+  
+  function ConsoleDetails({ userConsole, onClose }: ConsoleDetailsProps) {
+    const { t } = useTranslation();
+  
+    return (
+      <div className="fixed z-50 top-0 left-0 w-full h-full overflow-auto bg-opacity-75 bg-gray-900">
+        <div className="bg-bg-secondary rounded-lg w-full p-3 m-3 cursor-pointer select-none">
+          <button className="float-right" onClick={onClose}>
+            {t("Close")}
+          </button>
+          <div className="text-fs-h1 mt-5">{t('userConsolePage.lendTitle')}</div>
+                <hr className="pb-2" />
+                <div className="font-bold">{t('userConsolePage.lendAmount')}</div>
+                <div className="ml-3">{userConsole?.amount}</div>
+                <div className="font-bold">{t('userConsolePage.lendAccessories')}</div>
+                <div className="ml-3">{userConsole?.accessories}</div>
+        </div>
+      </div>
+    );
+  }
 
 export default ConsolesPage
